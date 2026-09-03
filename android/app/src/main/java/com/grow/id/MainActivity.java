b68307adce08c0742f10a0a716ba922ab891d8e9
Sophia <lorenscosta2@gmail.com>
2026-09-03 18:31:15 -0300

    Adiciona configuracao Android e atualiza projeto

---
+++ android/app/src/main/java/com/grow/id/MainActivity.java
@@ -0,0 +1,5 @@
+package com.grow.id;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,51 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Grow',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',    // 🔥 IMPORTANTE!
+    iosScheme: 'https'         // 🔥 IMPORTANTE!
+  }
 };
 
-export default config;
+export default config;PS C:\Users\sophi\Downloads\GROW-APP-DE-BOTANICA-main> Copy-Item ".\GROW-APP-DE-BOTANICA-main\*" "." -Recurse -Force
+Copy-Item : Não foi possível localizar uma parte do caminho 'C:\Users\sophi\Downloads\GROW-APP-DE-BOTANICA-main\GROW-A
+PP-DE-BOTANICA-main\android\app\build\intermediates\desugar_graph\debugAndroidTest\dexBuilderDebugAndroidTest\out\curr
+entProject\jar_471aea694e632c408cc1f6ba145eeb14ea264c6313cdd30f5494716438658be7_bucket_0'.
+No linha:1 caractere:1
++ Copy-Item ".\GROW-APP-DE-BOTANICA-main\*" "." -Recurse -Force
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (jar_471aea694e6...658be7_bucket_0:DirectoryInfo) [Copy-Item], DirectoryNot 
+   FoundException
+    + FullyQualifiedErrorId : CopyDirectoryInfoItemIOError,Microsoft.PowerShell.Commands.CopyItemCommand
+ 
+Copy-Item : Não foi possível localizar uma parte do caminho 'C:\Users\sophi\Downloads\GROW-APP-DE-BOTANICA-main\GROW-A
+PP-DE-BOTANICA-main\android\app\build\intermediates\desugar_graph\debugAndroidTest\dexBuilderDebugAndroidTest\out\curr
+entProject\jar_471aea694e632c408cc1f6ba145eeb14ea264c6313cdd30f5494716438658be7_bucket_1'.
+No linha:1 caractere:1
++ Copy-Item ".\GROW-APP-DE-BOTANICA-main\*" "." -Recurse -Force
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (jar_471aea694e6...658be7_bucket_1:DirectoryInfo) [Copy-Item], DirectoryNot 
+   FoundException
+    + FullyQualifiedErrorId : CopyDirectoryInfoItemIOError,Microsoft.PowerShell.Commands.CopyItemCommand
+ PS C:\Users\sophi\Downloads\GROW-APP-DE-BOTANICA-main> Copy-Item ".\GROW-APP-DE-BOTANICA-main\*" "." -Recurse -Force
+Copy-Item : Não foi possível localizar uma parte do caminho 'C:\Users\sophi\Downloads\GROW-APP-DE-BOTANICA-main\GROW-A
+PP-DE-BOTANICA-main\android\app\build\intermediates\desugar_graph\debugAndroidTest\dexBuilderDebugAndroidTest\out\curr
+entProject\jar_471aea694e632c408cc1f6ba145eeb14ea264c6313cdd30f5494716438658be7_bucket_0'.
+No linha:1 caractere:1
++ Copy-Item ".\GROW-APP-DE-BOTANICA-main\*" "." -Recurse -Force
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (jar_471aea694e6...658be7_bucket_0:DirectoryInfo) [Copy-Item], DirectoryNot 
+   FoundException
+    + FullyQualifiedErrorId : CopyDirectoryInfoItemIOError,Microsoft.PowerShell.Commands.CopyItemCommand
+ 
+Copy-Item : Não foi possível localizar uma parte do caminho 'C:\Users\sophi\Downloads\GROW-APP-DE-BOTANICA-main\GROW-A
+PP-DE-BOTANICA-main\android\app\build\intermediates\desugar_graph\debugAndroidTest\dexBuilderDebugAndroidTest\out\curr
+entProject\jar_471aea694e632c408cc1f6ba145eeb14ea264c6313cdd30f5494716438658be7_bucket_1'.
+No linha:1 caractere:1
++ Copy-Item ".\GROW-APP-DE-BOTANICA-main\*" "." -Recurse -Force
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (jar_471aea694e6...658be7_bucket_1:DirectoryInfo) [Copy-Item], DirectoryNot 
+   FoundException
+    + FullyQualifiedErrorId : CopyDirectoryInfoItemIOError,Microsoft.PowerShell.Commands.CopyItemCommand
+ 
